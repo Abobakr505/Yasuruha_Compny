f88d5698e463f5e5d41d80c6e337fb542073244a
Abobakr505 <abobakrhasan5335@gmail.com>
2025-12-24 21:00:31 +0200

    edit

--- src/pages/AdminProjects.tsx
+++ src/pages/AdminProjects.tsx
@@ -911,7 +911,7 @@ export default function AdminProjects() {
                     value={formData.icon_name}
                     onChange={(e) => setFormData({ ...formData, icon_name: e.target.value })}
                   >
-                    {['Code', 'ShoppingCart', 'Heart', 'Building2', 'Smartphone', 'Globe'].map((icon) => (
+                    {['Code', 'ShoppingCart', 'Heart', 'Building2', 'Smartphone', 'Globe' , 'User'].map((icon) => (
                       <option key={icon} value={icon}>{icon}</option>
                     ))}
                   </select>
